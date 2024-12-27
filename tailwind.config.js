8b0c9a66dd714dcfab309d8956e8edcbdbb67b8e
nanda <auliya190804@gmail.com>
2024-12-27 23:56:02 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#243642',
+        secondary: '#4DA1A9',
+        Almost: '#F6F4F0',
+      }
+    
+    },
+  },
+  plugins: [],
+}
+
